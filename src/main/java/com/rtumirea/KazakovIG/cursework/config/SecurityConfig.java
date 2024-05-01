40552d737f72e229c6f425496969940d2956c04d
RobotBot <robotbotyourube@gmail.com>
2024-05-01 16:56:15 +0300

    Permit all to all endPoints

--- src/main/java/com/rtumirea/KazakovIG/cursework/config/SecurityConfig.java
+++ src/main/java/com/rtumirea/KazakovIG/cursework/config/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/new-user").permitAll())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/new-user").permitAll()
+                        .requestMatchers("/**").permitAll())
                 .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                 .build();
     }
